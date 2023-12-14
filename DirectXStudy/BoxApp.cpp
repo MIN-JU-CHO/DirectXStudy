86f6898074cd215a64cdec17945c9a6326c9944e
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-12-14 20:22:47 +0900

    상자 예제

---
+++ DirectXStudy/BoxApp.cpp
@@ -0,0 +1,349 @@
+//****************************************************************************
+// BoxDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
+// 색깔 있는 상자의 렌더링
+// 조작 방법:
+// 왼쪽 버튼을 누른 채로 마우스를 움직이면 상자가 회전
+// 오른쪽 버튼을 누른 채로 마우스를 움직이면 상자가 확대/축소
+//****************************************************************************
+
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "MathHelper.h"
+
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+class BoxApp : public D3DApp
+{
+public:
+	BoxApp(HINSTANCE hInstance);
+	~BoxApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	void BuildGeometryBuffers();
+	void BuildFX();
+	void BuildVertexLayout();
+
+private:
+	ID3D11Buffer* mBoxVB;
+	ID3D11Buffer* mBoxIB;
+
+	ID3DX11Effect* mFX;
+	ID3DX11EffectTechnique* mTech;
+	ID3DX11EffectMatrixVariable* mfxWorldViewProj;
+
+	ID3D11InputLayout* mInputLayout;
+
+	XMFLOAT4X4 mWorld;
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+	// 디버그 빌드의 경우 실행시점 메모리 점검 기능을 활성화한다.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	BoxApp theApp(hInstance);
+
+	if (!theApp.Init())
+	{
+		return 0;
+	}
+
+	return theApp.Run();
+}
+// 생성자 정의
+BoxApp::BoxApp(HINSTANCE hInstance)
+	: D3DApp(hInstance), mBoxVB(0), mBoxIB(0), mFX(0), mTech(0),
+	mfxWorldViewProj(0), mInputLayout(0),
+	mTheta(1.5f * MathHelper::Pi), mPhi(0.25f * MathHelper::Pi), mRadius(5.0f)
+{
+	// window 제목 기본값 "D3D11 Application" -> "Box Demo" 로 변경
+	mMainWndCaption = L"Box Demo";
+
+	// 아직 초기화 하지 않은 구조체 멤버변수 초기화
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	// 아직 초기화 하지 않은 행렬 멤버변수 단위행렬로 초기화
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mWorld, I);
+	XMStoreFloat4x4(&mView, I);
+	XMStoreFloat4x4(&mProj, I);
+}
+
+BoxApp::~BoxApp()
+{
+	ReleaseCOM(mBoxVB);
+	ReleaseCOM(mBoxIB);
+	ReleaseCOM(mFX);
+	ReleaseCOM(mInputLayout);
+}
+
+bool BoxApp::Init()
+{
+	if (!D3DApp::Init())
+	{
+		return false;
+	}
+
+	BuildGeometryBuffers();
+	BuildFX();
+	BuildVertexLayout();
+
+	return true;
+}
+
+void BoxApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	// 창의 크기가 변했으므로 종횡비를 갱신(AspectRatio())하고
+	// 투영 행렬을 다시 계산(XMMatrixPerspectiveFovLH())한다.
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f * MathHelper::Pi,
+		AspectRatio(), 1.0f, 1000.0f);
+	// 결과는 멤버변수 mProj에 update
+	XMStoreFloat4x4(&mProj, P);
+}
+
+void BoxApp::UpdateScene(float dt)
+{
+	// 구면 좌표를 데카르트 좌표로 변환한다.
+	float x = mRadius * sinf(mPhi) * cosf(mTheta);
+	float z = mRadius * sinf(mPhi) * sinf(mTheta);
+	float y = mRadius * cosf(mPhi);
+
+	// 시야 행렬을 구축한다.
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f); // 점이므로 w = 1.0f
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	// 시야 행렬 결과는 멤버변수 mView에 update
+	XMStoreFloat4x4(&mView, V);
+}
+
+void BoxApp::DrawScene()
+{
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView,
+		reinterpret_cast<const float*>(&Colors::Blue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView,
+		D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	// 정점 구조체 입력 배치에 묶기, 기본도형 위상구조 설정
+	md3dImmediateContext->IASetInputLayout(mInputLayout);
+	md3dImmediateContext->IASetPrimitiveTopology(
+		D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	// 정점 버퍼 입력 슬롯에 묶기
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	md3dImmediateContext->IASetVertexBuffers(0, 1, &mBoxVB, &stride, &offset);
+	// 색인 버퍼 파이프라인에 묶기 - 입력 조립기 단계
+	md3dImmediateContext->IASetIndexBuffer(mBoxIB, DXGI_FORMAT_R32_UINT, 0);
+
+	// 상수들 설정
+	XMMATRIX world = XMLoadFloat4x4(&mWorld);
+	XMMATRIX view = XMLoadFloat4x4(&mView);
+	XMMATRIX proj = XMLoadFloat4x4(&mProj);
+	XMMATRIX worldViewProj = world * view * proj;
+	// 효과 객체의 상수 버퍼 변수(ex wvp행렬) update 
+	mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+	D3DX11_TECHNIQUE_DESC techDesc;
+	mTech->GetDesc(&techDesc);
+	for (UINT p = 0; p < techDesc.Passes; ++p)
+	{
+		mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+		// 색인 36개로 그린다.
+		md3dImmediateContext->DrawIndexed(36, 0, 0);
+	}
+
+	HR(mSwapChain->Present(0, 0));
+}
+
+void BoxApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void BoxApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void BoxApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		// 1 픽셀이 4분의 1도(degree 단위) 가 되게 한다.
+		// 움직인 픽셀 값 차이 * 0.25f
+		float dx = XMConvertToRadians(
+			0.25f * static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(
+			0.25f * static_cast<float>(y - mLastMousePos.y));
+		// 마우스 입력에 기초한 각도로 상자 주변의 궤도 카메라를 갱신한다.
+		mTheta += dx;
+		mPhi += dy;
+		// 각도를 mPhi로 한정한다.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btnState & MK_RBUTTON) != 0)
+	{
+		// 1 픽셀이 장면의 0.005 단위가 되게 한다.
+		float dx = 0.005f * static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.005f * static_cast<float>(y - mLastMousePos.y);
+		// 마우스 대각선 입력에 기초해서 궤도 카메라의 반지름을 갱신한다.
+		mRadius += dx - dy;
+		// 반지름 범위 제한
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
+	}
+	// 좌표 update
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void BoxApp::BuildGeometryBuffers()
+{
+	// 정점 버퍼를 생성한다.
+	Vertex vertices[] =
+	{
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), (const float*)&Colors::White },
+		{ XMFLOAT3(-1.0f, +1.0f, -1.0f), (const float*)&Colors::Black },
+		{ XMFLOAT3(+1.0f, +1.0f, -1.0f), (const float*)&Colors::Red },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f), (const float*)&Colors::Green },
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f), (const float*)&Colors::Blue },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f), (const float*)&Colors::Yellow },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f), (const float*)&Colors::Cyan },
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f), (const float*)&Colors::Magenta }
+	};
+	D3D11_BUFFER_DESC vbd;
+	vbd.ByteWidth = sizeof(Vertex) * 8; // 정육면체의 각 꼭짓점 8개
+	vbd.Usage = D3D11_USAGE_IMMUTABLE; // GPU Read Only
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER; // 정점 버퍼
+	vbd.CPUAccessFlags = 0; // GPU Read Only
+	vbd.MiscFlags = 0; // 정점 버퍼
+	vbd.StructureByteStride = 0;
+
+	// D3D11_SUBRESOURCE_DATA 사용법 (버퍼 초기화 자료)
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = vertices;
+	HR(md3dDevice->CreateBuffer(&vbd, &vinitData, &mBoxVB));
+	// 이후에 DrawScene() 을 호출하여 입력 슬롯에 묶는다.
+
+	// 색인 버퍼를 생성한다.
+	UINT indices[] = {
+		// 상자의 앞면
+		0, 1, 2,
+		0, 2, 3,
+		// 뒷면
+		4, 6, 5,
+		4, 7, 6,
+		// 왼쪽 면
+		4, 5, 1,
+		4, 1, 0,
+		// 오른쪽 면
+		3, 2, 6,
+		3, 6, 7,
+		// 윗면
+		1, 5, 6,
+		1, 6, 2,
+		// 밑면
+		4, 0, 3,
+		4, 3, 7
+	};
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.ByteWidth = sizeof(UINT) * 36; // 면 12개당 색인 3개씩 총 36개
+	ibd.Usage = D3D11_USAGE_IMMUTABLE; // GPU Read Only
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER; // 색인 버퍼
+	ibd.CPUAccessFlags = 0; // GPU Read Only
+	ibd.MiscFlags = 0; // 색인 버퍼
+	ibd.StructureByteStride = 0;
+
+	// D3D11_SUBRESOURCE_DATA 사용법 (버퍼 초기화 자료)
+	D3D11_SUBRESOURCE_DATA iinitData;
+	vinitData.pSysMem = indices;
+
+	// 색인 버퍼를 생성
+	HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mBoxIB));
+}
+
+void BoxApp::BuildFX()
+{
+	// 효과 컴파일
+	DWORD shaderFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+	shaderFlags |= D3D10_SHADER_DEBUG;
+	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
+#endif
+	ID3D10Blob* compiledShader = 0;
+	ID3D10Blob* compilationMsgs = 0;
+	HRESULT hr = D3DX11CompileFromFile(L"FX/color.fx", 0, 0, 0,
+		"fx_5_0", shaderFlags, 0, 0,
+		&compiledShader, &compilationMsgs, 0);
+	// compilationMsgs 오류나 경고 메시지가 저장되어있을 수도 있다.
+	if (compilationMsgs != 0)
+	{
+		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
+		ReleaseCOM(compilationMsgs);
+	}
+
+	// compilationMsgs 에 오류 메시지가 없었다고 해도
+	// 컴파일 오류 여부를 명시적으로 점검하는 것이 바람직하다.
+	if (FAILED(hr))
+	{
+		//assert(true);
+		DXTrace(__FILE__, (DWORD)__LINE__, hr, L"D3DX11CompileFromFile", true);
+	}
+
+	HR(D3DX11CreateEffectFromMemory(
+		compiledShader->GetBufferPointer(),
+		compiledShader->GetBufferSize(),
+		0, md3dDevice, &mFX));
+	// 컴파일된 셰이더 자료를 다 사용했으므로 해제 (mFX 는 소멸자에서 해제해줌)
+	ReleaseCOM(compiledShader);
+	// 효과 상수 버퍼 변경 위해 미리 포인터 변수 저장
+	mTech = mFX->GetTechniqueByName("ColorTech");
+	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void BoxApp::BuildVertexLayout()
+{
+	// 정점 입력 배치를 만든다.
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+	// 입력 배치 객체를 생성한다.
+	D3DX11_PASS_DESC passDesc;
+	mTech->GetPassByIndex(0)->GetDesc(&passDesc);
+	HR(md3dDevice->CreateInputLayout(vertexDesc, 2, passDesc.
+		pIAInputSignature, passDesc.IAInputSignatureSize, &mInputLayout));
+	// 이후에 DrawScene()에서 입력배치를 묶는다.
+}
